2f74e0b6412aea649ac0d88db6d9f17077799c8e
YuSeokHyun <fkstndnjs@naver.com>
2022-12-06 16:57:21 +0900

    fix

---
+++ src/data/tweet.data.js
@@ -0,0 +1,13 @@
+let tweets = [
+    {
+        id: "1",
+        createdAt: "2022-12-01",
+        name: "SeokHyun Yu",
+        username: "ysh",
+        text: "Hello",
+    },
+];
+
+export const getAllTweets = () => {
+    return tweets;
+};
--- src/router/tweet.router.js
+++ src/router/tweet.router.js
@@ -1,71 +1,62 @@
 import express from "express";
+import * as tweetRepository from "../data/tweet.data.js";
 
 const tweetRouter = express.Router();
 
-let tweets = [
-  {
-    id: "1",
-    createdAt: "2022-12-01",
-    name: "SeokHyun Yu",
-    username: "ysh",
-    text: "Hello",
-  },
-];
-
 // 전체 조회
 tweetRouter.get("/", (req, res) => {
-  const tempTweets = req.query.username
-    ? tweets.filter((tweet) => tweet.username === req.query.username)
-    : tweets;
+    const tempTweets = req.query.username
+        ? tweets.filter((tweet) => tweet.username === req.query.username)
+        : tweetRepository.getAllTweets();
 
-  res.status(200).json(tempTweets);
+    res.status(200).json(tempTweets);
 });
 
 // 상세 조회
 tweetRouter.get("/:id", (req, res) => {
-  const { id } = req.params;
-  const tweet = tweets.find((tweet) => tweet.id === id);
+    const { id } = req.params;
+    const tweet = tweets.find((tweet) => tweet.id === id);
 
-  if (tweet) {
-    res.status(200).json(tweet);
-  } else {
-    res.status(404).json({ status: 404, message: `id ${id} NOT FOUND` });
-  }
+    if (tweet) {
+        res.status(200).json(tweet);
+    } else {
+        res.status(404).json({ status: 404, message: `id ${id} NOT FOUND` });
+    }
 });
 
 // 트윗 생성
 tweetRouter.post("/", (req, res) => {
-  tweets = [req.body, ...tweets];
+    tweets = [req.body, ...tweets];
 
-  res.status(201).json(tweets);
+    res.status(201).json(tweets);
 });
 
 // 트윗 수정
 tweetRouter.put("/:id", (req, res) => {
-  const { id } = req.params;
-  const { text } = req.body;
-  const tweet = tweets.find((tweet) => tweet.id === id);
+    const { id } = req.params;
+    const { text } = req.body;
+    const tweet = tweets.find((tweet) => tweet.id === id);
 
-  if (tweet) {
-    tweet.text = text;
+    if (tweet) {
+        tweet.text = text;
 
-    res.status(200).json(tweet);
-  } else {
-    res.status(404).json({ status: 404, message: `id ${id} NOT FOUND` });
-  }
+        res.status(200).json(tweet);
+    } else {
+        res.status(404).json({ status: 404, message: `id ${id} NOT FOUND` });
+    }
 });
 
 // 트윗 삭제
 tweetRouter.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  const prevLength = tweets.length;
-  tweets = tweets.filter((tweet) => tweet.id !== id);
+    const { id } = req.params;
+    const prevLength = tweets.length;
+    tweets = tweets.filter((tweet) => tweet.id !== id);
 
-  if (prevLength !== tweets.length) {
-    res.sendStatus(204);
-  } else {
-    res.status(404).json({ status: 404, message: `id ${id} NOT FOUND` });
-  }
+    if (prevLength !== tweets.length) {
+        res.sendStatus(204);
+    } else {
+        res.status(404).json({ status: 404, message: `id ${id} NOT FOUND` });
+    }
 });
 
 export default tweetRouter;
