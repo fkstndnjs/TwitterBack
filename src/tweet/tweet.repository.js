c98c917f21c07e2c389c8db4d9cd150e79529195
fkstndnjs <fkstndnjs@naver.com>
2022-12-07 22:27:48 +0900

    fix

--- src/tweet/tweet.repository.js
+++ src/tweet/tweet.repository.js
@@ -1,53 +1,53 @@
 let tweets = [
-    {
-        id: 1,
-        createdAt: new Date().toLocaleString(),
-        name: "SeokHyun Yu",
-        username: "ysh",
-        text: "Hello",
-    },
+  {
+    id: 1,
+    createdAt: new Date().toLocaleString(),
+    name: "SeokHyun Yu",
+    username: "ysh",
+    text: "Hello",
+  },
 ];
 
 export const getAllTweets = async () => {
-    return tweets;
+  return tweets;
 };
 
 export const getAllTweetsByUsername = async (username) => {
-    return tweets.filter((tweet) => tweet.username === username);
+  return tweets.filter((tweet) => tweet.username === username);
 };
 
 export const getTweetById = async (id) => {
-    return tweets.find((tweet) => `${tweet.id}` === id);
+  return tweets.find((tweet) => `${tweet.id}` === id);
 };
 
 export const createTweet = async ({ name, username, text }) => {
-    const newTweet = {
-        id: (tweets[0]?.id || 0) + 1,
-        createdAt: Date.now().toLocaleString(),
-        name,
-        username,
-        text,
-    };
+  const newTweet = {
+    id: (tweets[0]?.id || 0) + 1,
+    createdAt: Date.now().toLocaleString(),
+    name,
+    username,
+    text,
+  };
 
-    tweets = [newTweet, ...tweets];
+  tweets = [newTweet, ...tweets];
 
-    return tweets;
+  return tweets;
 };
 
 export const updateTweet = async (id, text) => {
-    const tweet = tweets.find((tweet) => `${tweet.id}` === id);
+  const tweet = tweets.find((tweet) => `${tweet.id}` === id);
 
-    if (tweet) {
-        tweet.text = text;
-    }
+  if (tweet) {
+    tweet.text = text;
+  }
 
-    return tweet;
+  return tweet;
 };
 
 export const deleteTweet = async (id) => {
-    const prevLength = tweets.length;
+  const prevLength = tweets.length;
 
-    tweets = tweets.filter((tweet) => `${tweet.id}` !== id);
+  tweets = tweets.filter((tweet) => `${tweet.id}` !== id);
 
-    return prevLength !== tweets.length;
+  return prevLength !== tweets.length;
 };
