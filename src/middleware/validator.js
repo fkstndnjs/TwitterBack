bd10289f1c48a6b649d16bbb427ca86e3cb46efd
fkstndnjs <fkstndnjs@naver.com>
2022-12-07 22:53:55 +0900

    fix

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -1,7 +1,7 @@
 import { validationResult } from "express-validator";
 
 export const validate = (req, res, next) => {
-  // "express-validator"에서 req에 넘겨준 validation 결과를 validationResult()로 가져옴
+  // "express-validator"에서 req에 넘겨준 validation 결과를 validationResult()로 가져올 수 있다
   const errors = validationResult(req);
 
   if (errors.isEmpty()) {
