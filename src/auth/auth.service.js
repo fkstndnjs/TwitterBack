dd5f55ec39df0b31059b1ff4056f88f808937a42
YuSeokHyun <fkstndnjs@naver.com>
2022-12-08 11:02:50 +0900

    fix

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -1,5 +1,9 @@
 import * as authRepository from "./auth.repository.js";
 
+const jwtSecretKey = "secret";
+const jwtExpiresInDays = "2d";
+const bcryptSaltRounds = 12;
+
 export const signup = (req, res, next) => {};
 
 export const login = (req, res, next) => {};
