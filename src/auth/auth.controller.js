e50097009e3db511dac90a8e3cfca23fb6d43a09
YuSeokHyun <fkstndnjs@naver.com>
2022-12-08 10:41:39 +0900

    fix

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,14 +1,24 @@
 import express from "express";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.js";
 import * as authService from "./auth.service.js";
 
 const authController = express.Router();
 
 // validation
+const loginValidation = [
+    body("username").trim().notEmpty().withMessage("username을 입력해주세요."),
+    body("password")
+        .trim()
+        .isLength({ min: 5 })
+        .withMessage("비밀번호는 최소 5글자 이상 입력해주세요."),
+    validate(),
+];
 
 // 회원가입
 authController.post("/signup", authService.signup);
 
 // 로그인
-authController.post("/login", authService.login);
+authController.post("/login", loginValidation, authService.login);
 
 export default authController;
