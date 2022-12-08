57f034d4e67c609b9b8e77500482a4aa73b10ccc
YuSeokHyun <fkstndnjs@naver.com>
2022-12-08 15:48:46 +0900

    fix

--- src/app.js
+++ src/app.js
@@ -5,22 +5,28 @@ import cors from "cors";
 import tweetController from "./tweet/tweet.controller.js";
 import authController from "./auth/auth.controller.js";
 
+// 서버 생성
 const app = express();
 
+// 미들웨어
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 
+// 라우터
 app.use("/tweet", tweetController);
 app.use("/auth", authController);
 
+// 404 에러 핸들러
 app.use((req, res, next) => {
     res.sendStatus(404);
 });
 
+// 500 에러 핸들러
 app.use((err, req, res, next) => {
     res.sendStatus(500);
 });
 
+// 8000 포트로 listen
 app.listen(8000);
