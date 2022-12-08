331a62d9a727ac5ad5b103aa4629feb7e5f0c1a0
YuSeokHyun <fkstndnjs@naver.com>
2022-12-08 10:38:18 +0900

    fix

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -3,7 +3,10 @@ import * as authService from "./auth.service.js";
 
 const authController = express.Router();
 
+// 회원가입
 authController.post("/signup", authService.signup);
+
+// 로그인
 authController.post("/login", authService.login);
 
 export default authController;
