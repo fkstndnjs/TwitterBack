8169b27a68819b28018c160f668f7066f8ee829d
YuSeokHyun <fkstndnjs@naver.com>
2022-12-08 11:05:50 +0900

    fix

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -1,10 +1,17 @@
+import * as userRepository from "../user/user.repository.js";
+
 const jwtSecretKey = "secret";
 const jwtExpiresInDays = "2d";
 const bcryptSaltRounds = 12;
 
 export const signup = (req, res, next) => {
     const { username, password, name, email } = req.body;
-    const user = await;
+    const user = userRepository.findByUsername(username);
+
+    // 이미 가입된 유저일 경우
+    if (user) {
+        return res.status(409).json({ message: `이미 가입된 유저입니다.` });
+    }
 };
 
 export const login = (req, res, next) => {};
