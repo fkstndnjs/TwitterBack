ac1a95fa7f8942dfb375261eced00420e1e0d9b6
YuSeokHyun <fkstndnjs@naver.com>
2022-12-08 11:06:54 +0900

    fix

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -1,10 +1,11 @@
 import * as userRepository from "../user/user.repository.js";
+import bcrypt from "bcrypt";
 
 const jwtSecretKey = "secret";
 const jwtExpiresInDays = "2d";
 const bcryptSaltRounds = 12;
 
-export const signup = (req, res, next) => {
+export const signup = async (req, res, next) => {
     const { username, password, name, email } = req.body;
     const user = userRepository.findByUsername(username);
 
@@ -12,6 +13,8 @@ export const signup = (req, res, next) => {
     if (user) {
         return res.status(409).json({ message: `이미 가입된 유저입니다.` });
     }
+
+    const hashed = bcrypt.hash();
 };
 
 export const login = (req, res, next) => {};
