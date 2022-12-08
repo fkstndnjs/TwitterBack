8e417ed8f01a885b57dee494e70b6d78b42aff88
YuSeokHyun <fkstndnjs@naver.com>
2022-12-08 14:56:39 +0900

    fix

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -7,20 +7,26 @@ export const auth = async (req, res, next) => {
     if (authHeader && authHeader.startsWith("Bearer ")) {
         const token = authHeader.split(" ")[1];
 
-        jwt.verify(token, "secret", (err, data) => {
+        jwt.verify(token, "secret", async (err, data) => {
             if (err) {
                 return res.status(401).json({ message: "인증 에러" });
             }
 
-            const user = userRepository.findById(data.id);
+            const user = await userRepository.findById(data.id);
 
             if (user) {
+                console.log("🚀 ---------------🚀");
+                console.log("🚀 ~ 1");
+                console.log("🚀 ---------------🚀");
                 req.token = token;
                 req.userId = user.id;
 
                 return next();
             }
 
+            console.log("🚀 ---------------🚀");
+            console.log("🚀 ~ 2");
+            console.log("🚀 ---------------🚀");
             return res.status(401).json({ message: "인증 에러" });
         });
     }
