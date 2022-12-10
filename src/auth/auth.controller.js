7bb262cd694ddb674d57ef9a69f90b30bdc9af83
fkstndnjs <fkstndnjs@naver.com>
2022-12-10 16:14:02 +0900

    fix

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -8,22 +8,22 @@ const authController = express.Router();
 
 // validation
 const loginValidation = [
-    body("username").trim().notEmpty().withMessage("username을 입력해주세요."),
-    body("password")
-        .trim()
-        .isLength({ min: 5 })
-        .withMessage("비밀번호는 최소 5글자 이상 입력해주세요."),
-    validate,
+  body("username").trim().notEmpty().withMessage("username을 입력해주세요."),
+  body("password")
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage("비밀번호는 최소 5글자 이상 입력해주세요."),
+  validate,
 ];
 
 const signUpValidation = [
-    ...loginValidation,
-    body("name").trim().notEmpty().withMessage("이름이 비어있습니다."),
-    body("email")
-        .isEmail()
-        .normalizeEmail()
-        .withMessage("이메일 형식에 맞지 않습니다"),
-    validate,
+  ...loginValidation,
+  body("name").trim().notEmpty().withMessage("이름이 비어있습니다."),
+  body("email")
+    .isEmail()
+    .normalizeEmail()
+    .withMessage("이메일 형식에 맞지 않습니다"),
+  validate,
 ];
 
 // 회원가입
