6dc7a0a1d4d5459be24c218d876898e8dc357683
fkstndnjs <fkstndnjs@naver.com>
2022-12-10 16:13:42 +0900

    fix

--- src/tweet/tweet.repository.js
+++ src/tweet/tweet.repository.js
@@ -1,62 +1,60 @@
 import * as userRepository from "../user/user.repository.js";
 
 let tweets = [
-    {
-        createdAt: new Date().toLocaleString(),
-        id: 1,
-        text: "Hello",
-        userId: 1,
-    },
+  {
+    createdAt: new Date().toLocaleString(),
+    id: 1,
+    text: "Hello",
+    userId: 1,
+  },
 ];
 
 export const getAllTweets = async () => {
-    return Promise.all(
-        tweets.map(async (tweet) => {
-            const { username, name } = await userRepository.findById(
-                tweet.userId
-            );
+  return Promise.all(
+    tweets.map(async (tweet) => {
+      const { username, name } = await userRepository.findById(tweet.userId);
 
-            return { ...tweet, username, name };
-        })
-    );
+      return { ...tweet, username, name };
+    })
+  );
 };
 
 export const getAllTweetsByUsername = async (username) => {
-    return getAllTweets().then((tweets) =>
-        tweets.filter((tweet) => tweet.username === username)
-    );
+  return getAllTweets().then((tweets) =>
+    tweets.filter((tweet) => tweet.username === username)
+  );
 };
 
 export const getTweetById = async (id) => {
-    const tweet = tweets.find((tweet) => `${tweet.id}` === id);
-    const { username, name } = userRepository.findById(tweet.userId);
+  const tweet = tweets.find((tweet) => `${tweet.id}` === id);
+  const { username, name } = userRepository.findById(tweet.userId);
 
-    return { ...tweet, username, name };
+  return { ...tweet, username, name };
 };
 
 export const createTweet = async (text, userId) => {
-    const newTweet = {
-        id: (tweets[0]?.id || 0) + 1,
-        createdAt: Date.now().toLocaleString(),
-        text,
-        userId,
-    };
+  const newTweet = {
+    id: (tweets[0]?.id || 0) + 1,
+    createdAt: Date.now().toLocaleString(),
+    text,
+    userId,
+  };
 
-    tweets = [newTweet, ...tweets];
+  tweets = [newTweet, ...tweets];
 
-    return newTweet;
+  return newTweet;
 };
 
 export const updateTweet = async (id, text) => {
-    const tweet = tweets.find((tweet) => `${tweet.id}` === id);
+  const tweet = tweets.find((tweet) => `${tweet.id}` === id);
 
-    if (tweet) {
-        tweet.text = text;
-    }
+  if (tweet) {
+    tweet.text = text;
+  }
 
-    return tweet;
+  return tweet;
 };
 
 export const deleteTweet = async (id) => {
-    tweets = tweets.filter((tweet) => `${tweet.id}` !== id);
+  tweets = tweets.filter((tweet) => `${tweet.id}` !== id);
 };
