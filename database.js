1bb9c67218f01ff9951a6c36e67b29f979470e1b
fkstndnjs <fkstndnjs@naver.com>
2022-12-11 21:02:29 +0900

    fix

--- database.js
+++ database.js
@@ -1,12 +1,13 @@
 import mysql from "mysql2";
+import sequelize from "sequelize";
 
-const pool = mysql.createPool({
-  host: "127.0.0.1",
-  user: "root",
-  database: "twitter",
-  password: "0000",
-});
+// const pool = mysql.createPool({
+//   host: "127.0.0.1",
+//   user: "root",
+//   database: "twitter",
+//   password: "0000",
+// });
 
-const db = pool.promise();
+// const db = pool.promise();
 
-export default db;
+// export default db;
