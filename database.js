71f1aa3f9b456c0de040b86fb3cd44799a0c6123
fkstndnjs <fkstndnjs@naver.com>
2022-12-11 21:05:50 +0900

    fix

--- database.js
+++ database.js
@@ -1,5 +1,9 @@
 import mysql from "mysql2";
-import sequelize from "sequelize";
+import sequel from "sequelize";
+
+const sequelize = new sequel.Sequelize("twitter", "root", "0000", {
+  host: "127.0.0.1",
+});
 
 // const pool = mysql.createPool({
 //   host: "127.0.0.1",
