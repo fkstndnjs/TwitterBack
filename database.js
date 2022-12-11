31c3e470da73ea111a98eb21cf7906909cdf4489
fkstndnjs <fkstndnjs@naver.com>
2022-12-11 21:13:37 +0900

    fix

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
-import sequel from "sequelize";
+import { Sequelize } from "sequelize";
 
-const sequelize = new sequel.Sequelize("twitter", "root", "0000", {
+const sequelize = new Sequelize("twitter", "root", "0000", {
   host: "127.0.0.1",
   dialect: "mysql",
 });
