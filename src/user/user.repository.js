9eb60d0effac39d1417d4189f0d1c1f61fd94860
fkstndnjs <fkstndnjs@naver.com>
2022-12-11 12:32:37 +0900

    fix

--- src/user/user.repository.js
+++ src/user/user.repository.js
@@ -1,28 +1,30 @@
 import bcrypt from "bcrypt";
+import db from "../../database.js";
 
 let users = [
-    {
-        createdAt: new Date().toLocaleString(),
-        id: 1,
-        username: "ysh",
-        password: bcrypt.hashSync("yshysh", 12),
-        name: "SeokHyun Yu",
-        email: "fkstndnjs@naver.com",
-    },
+  {
+    createdAt: new Date().toLocaleString(),
+    id: 1,
+    username: "ysh",
+    password: bcrypt.hashSync("yshysh", 12),
+    name: "SeokHyun Yu",
+    email: "fkstndnjs@naver.com",
+  },
 ];
 
 export const findByUsername = async (username) => {
-    return users.find((user) => user.username === username);
+  return users.find((user) => user.username === username);
 };
 
 export const findById = async (id) => {
-    return users.find((user) => user.id === id);
+  return users.find((user) => user.id === id);
 };
 
 export const createUser = async (user) => {
-    const createdUser = { id: users[users.length - 1]?.id + 1 || 1, ...user };
+  const { username, password, name, email } = user;
 
-    users.push(createdUser);
-
-    return createdUser;
+  return db.execute(
+    `INSERT INTO user(username, password, name, email) VALUES(?, ?, ?, ?)`,
+    [username, password, name, email]
+  );
 };
