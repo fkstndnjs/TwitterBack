4a865776e10f882b3726f793fac3dca95108087a
YuSeokHyun <fkstndnjs@naver.com>
2022-12-12 14:04:38 +0900

    fix

--- src/middleware/rate-limiter.js
+++ src/middleware/rate-limiter.js
@@ -3,5 +3,11 @@ import rateLimiter from "express-rate-limit";
 export default rateLimiter({
     windowMs: 10 * 1000,
     max: 10,
-    message: "Too Many Request",
+    message: "요청이 너무 많습니다.",
+    handler: (req, res, next, options) => {
+        console.log("options.statusCode -", options.statusCode);
+        console.log("options.message -", options.message);
+
+        res.status(options.statusCode).send(options.message);
+    },
 });
