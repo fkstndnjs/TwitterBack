7b46e518967fcbf83217323ee48a66f0263f2cf1
YuSeokHyun <fkstndnjs@naver.com>
2022-12-12 11:33:28 +0900

    fix

--- src/middleware/csrf.js
+++ src/middleware/csrf.js
@@ -1,5 +1,21 @@
-export const csrfCheck = (req, res, next) => {
+import bcrypt from "bcrypt";
+
+export const csrfCheck = async (req, res, next) => {
     if (req.method === "GET") {
         return next();
     }
+
+    const csrfHeader = req.get("csrf-token");
+
+    if (!csrfHeader) {
+        return res.status(403).json({ message: "CSRF ERROR" });
+    }
+
+    const isTrue = await bcrypt.compare("csrf", csrfHeader);
+
+    if (!isTrue) {
+        return res.status(403).json({ message: "CSRF ERROR" });
+    }
+
+    next();
 };
