05cdd05de293f8efc8053f6611a2dbc073384228
YuSeokHyun <fkstndnjs@naver.com>
2022-12-12 11:22:29 +0900

    fix

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  jwtSecretKey: process.env.JWT_SECRET_KEY,
-  jwtExpires: process.env.JWT_EXPIRES,
-  bcryptSaltRounds: parseInt(process.env.BCRYPT_SALT_ROUNDS),
-  port: parseInt(process.env.SERVER_PORT),
+    jwtSecretKey: process.env.JWT_SECRET_KEY,
+    jwtExpires: process.env.JWT_EXPIRES,
+    bcryptSaltRounds: parseInt(process.env.BCRYPT_SALT_ROUNDS),
+    port: parseInt(process.env.SERVER_PORT),
 };
