4453726c6128737f332c8ca77bb4f8cdaab67351
fkstndnjs <fkstndnjs@naver.com>
2022-12-12 22:00:13 +0900

    fix

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ const options = {
   // import swaggerDefinitions
   swaggerDefinition,
   // path to the API docs
-  apis: ["./tweet/*.js"],
+  apis: ["./*.js"],
 };
 
 // initialize swagger-jsdoc
